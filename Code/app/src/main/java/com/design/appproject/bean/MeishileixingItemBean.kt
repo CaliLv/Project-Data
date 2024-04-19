6a5b72d50df300701242b6d42d0d7ebb11b110b4
Cali <148566444+CaliLv@users.noreply.github.com>
2024-04-19 21:48:18 +0800

    Add files via upload

---
+++ Code/app/src/main/java/com/design/appproject/bean/MeishileixingItemBean.kt
@@ -0,0 +1,10 @@
+package com.design.appproject.bean
+
+/**
+ * 美食类型实体类
+ */
+data class MeishileixingItemBean(
+    var id:Long=0L,
+    var meishileixing:String="",
+    var addtime:String?=null,
+)
