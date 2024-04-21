258a740c8f6230de3dc3968e2b9947745d4cbe6a
Cali <148566444+CaliLv@users.noreply.github.com>
2024-04-21 10:33:41 +0800

    Add files via upload

---
+++ Code/app/src/main/java/com/design/appproject/logic/viewmodel/yongcanjihua/ListModel.kt
@@ -0,0 +1,36 @@
+package com.design.appproject.logic.viewmodel.yongcanjihua
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.design.appproject.bean.YongcanjihuaItemBean
+import com.design.appproject.logic.repository.HomeRepository
+import java.io.File
+/**用餐计划viewmodel*/
+class ListModel : ViewModel() {
+
+    private val pageData = MutableLiveData<List<Any>>()
+
+    val pageLiveData = Transformations.switchMap(pageData) { request ->
+        pageData.value?.let {
+            HomeRepository.page<YongcanjihuaItemBean>(it[0] as String, it[1] as Map<String, String>)
+        }
+    }
+
+    fun page(tableName: String, map: Map<String,String>) {
+        pageData.value = listOf(tableName, map)
+    }
+
+
+    private val listData = MutableLiveData<List<Any>>()
+
+    val listLiveData = Transformations.switchMap(listData) { request ->
+        listData.value?.let {
+            HomeRepository.list<YongcanjihuaItemBean>(it[0] as String, it[1] as Map<String, String>)
+        }
+    }
+
+    fun list(tableName: String, map: Map<String,String>) {
+        listData.value = listOf(tableName, map)
+    }
+}
